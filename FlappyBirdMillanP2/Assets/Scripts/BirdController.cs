9248e9e5ff728ae2c672d59b4f03289fba400d32
maksuk-star <aedenmillan64@gmail.com>
2024-12-09 10:23:04 -0800

    added flaps need to
    added animations need to finsish it

---
+++ FlappyBirdMillanP2/Assets/Scripts/BirdController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public float upForce = 200f;
+    
+    private bool isDead = false;
+
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown (0))
+                    {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                    }
+        }
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        isDead = true;
+    }
+
+
+
+
+
+
+
+
+}
+
