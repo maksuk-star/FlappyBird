9c1cd1be370d7f9ebd351703638806ad0e74d54d
maksuk-star <aedenmillan64@gmail.com>
2024-12-11 09:59:25 -0800

    finished animating and scoring count
    need to do video 6-10

--- FlappyBirdMillanP2/Assets/Scripts/BirdController.cs
+++ FlappyBirdMillanP2/Assets/Scripts/BirdController.cs
@@ -9,9 +9,11 @@ public class BirdController : MonoBehaviour
     private bool isDead = false;
 
     private Rigidbody2D rb2d;
+    private Animator anim;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,24 +22,18 @@ public class BirdController : MonoBehaviour
         if (isDead == false)
         {
             if (Input.GetMouseButtonDown (0))
-                    {
+            {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
-                    }
+                anim.SetTrigger ("Flap");
+            }
         }
     }
 
     private void OnCollisionEnter2D()
     {
         isDead = true;
+        anim.SetTrigger ("Dead");
     }
-
-
-
-
-
-
-
-
 }
 
